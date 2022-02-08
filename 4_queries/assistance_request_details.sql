7c252abfd57f7fca354f45ca0c445628482dafaf
Melissa <mawilliamson10@gmail.com>
2022-02-08 22:12:53 +0000

    assistance details

---
+++ 4_queries/assistance_request_details.sql
@@ -0,0 +1,21 @@
+-- Get important data about each assistance request.
+-- Select the teacher's name, student's name, assignment's name, and the duration of each assistance request.
+-- Subtract completed_at by started_at to find the duration.
+-- Order by the duration of the request.
+-- teacher       |         student          |    assignment    | duration 
+--------------------+--------------------------+------------------+----------
+--  Helmer Rodriguez   | Maximillian Pfannerstill | Expedita officia | 00:02:45
+--  Roberto Towne      | Vivien Mosciski          | Ea totam iste    | 00:02:45
+--  Georgiana Fahey    | Gene Carter              | Ut fuga          | 00:02:45
+--  Cheyanne Powlowski | Vivien Mosciski          | Eum eaque        | 00:02:45
+--  Roberto Towne      | Maximillia Willms        | Quibusdam est    | 00:03:00
+-- (26299 rows)
+
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, ((assistance_requests.completed_at) - (assistance_requests.started_at)) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN assignments ON assignments.id = assistance_requests.assignment_id
+ORDER BY duration;
+
+
