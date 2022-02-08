b4f3cbd27c41d837bf1e06eb7f31a1bfa3304fbc
Melissa <mawilliamson10@gmail.com>
2022-02-08 01:28:22 +0000

    students that finish their work quicker than expected

---
+++ 3_queries_group-by/low_average.sql
@@ -0,0 +1,15 @@
+-- Get the students who's average time it takes to complete an assignment is less than the average estimated time it takes to complete an assignment.
+
+-- Select the name of the student, their average completion time, and the average suggested completion time.
+-- Order by average completion time from smallest to largest.
+-- Only get currently enrolled students.
+-- This will require data from students, assignment_submissions, and assignments.
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
